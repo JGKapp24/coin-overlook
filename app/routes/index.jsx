d074b34a04d92b73332bc78689f928c4d343d438
Joel Kapp <joel.g.kapp@gmail.com>
2022-03-22 17:45:51 -0600

    Fetch list of exchanges

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,15 @@
+import { json, useLoaderData } from "remix";
 import Header from '../components/Header';
 
+export const loader = async () => {
+  const data = await fetch('https://api.coingecko.com/api/v3/exchanges?per_page=10');
+  const exchanges = await data.json();
+  return json({ exchanges });
+};
+
 export default function Index() {
+  const { exchanges } = useLoaderData();
+  console.log(exchanges);
   return (
     <div className="">
       <Header />
