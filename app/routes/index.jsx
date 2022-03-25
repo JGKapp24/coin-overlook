c210d73cc10f074f64fe6600af1844e51a45b2c9
Joel Kapp <joel.g.kapp@gmail.com>
2022-03-24 22:16:51 -0600

    Change number of exchanges to 20 in list

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -4,7 +4,7 @@ import ExchangeList from "../components/ExchangeList";
 
 export const loader = async () => {
   const data = await fetch(
-    "https://api.coingecko.com/api/v3/exchanges?per_page=10"
+    "https://api.coingecko.com/api/v3/exchanges?per_page=20"
   );
   const exchanges = await data.json();
   return json({ exchanges });
