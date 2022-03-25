871b4fa94d85819797d31fabc18e24c5f7409303
Joel Kapp <joel.g.kapp@gmail.com>
2022-03-24 22:01:16 -0600

    Add logo and favicon

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,8 +4,9 @@ export default function Header() {
   return (
     <div className="bg-gray-800 text-gray-50 px-6 py-4 md:px-10 md:py-8">
       <h1 className="text-4xl font-semibold">
-        <NavLink to="/">
-          Coin Overlook
+        <NavLink to="/" className="flex flex-row items-center">
+          <img alt="logo" src="/logo.svg" className="w-10 h-10 inline" />
+          <span className="ml-2">Coin Overlook</span>
         </NavLink>
       </h1>
     </div>
