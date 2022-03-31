3ab7f6fcd2fb96fd2799ff878c21bd62b5911383
Joel Kapp <joel.g.kapp@gmail.com>
2022-03-31 08:16:24 -0600

    Very first pass at pagination

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,17 +1,21 @@
-import { json, useLoaderData } from "remix";
+import { json, useLoaderData , Link} from "remix";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import Header from "../components/Header";
 import ExchangeList from "../components/ExchangeList";
 
-export const loader = async () => {
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page");
   const data = await fetch(
-    "https://api.coingecko.com/api/v3/exchanges?per_page=20"
+    // Need pageNum
+    `https://api.coingecko.com/api/v3/exchanges?per_page=10&page=${page}`
   );
   const exchanges = await data.json();
-  return json({ exchanges });
+  return json({ exchanges, pageNum: +page || 1 });
 };
 
 export default function Index() {
-  const { exchanges } = useLoaderData();
+  const { exchanges, pageNum } = useLoaderData();
 
   return (
     <div className="">
@@ -19,6 +23,21 @@ export default function Index() {
       <div className="py-3">
         <ExchangeList exchanges={exchanges} />
       </div>
+      <div className="flex flex-row items-center">
+        {pageNum > 1 && (
+          <Link
+            to={`?page=${pageNum - 1}`}
+          >
+            <ChevronLeftIcon className="w-5 h-5" />
+          </Link>
+        )}
+        <div>{pageNum}</div>
+        <Link
+          to={`?page=${pageNum + 1}`}
+        >
+          <ChevronRightIcon className="w-5 h-5" />
+        </Link>
+      </div>´
     </div>
   );
 }
